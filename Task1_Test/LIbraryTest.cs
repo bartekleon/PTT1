a57f16dd33ed45a478789da36222579edef7a546
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2020-11-16 15:21:59 +0100

    tests: remove and add

--- Task1_Test/LIbraryTest.cs
+++ Task1_Test/LIbraryTest.cs
@@ -4,13 +4,46 @@ using PT_Task1.DataLayer;
 namespace Task1_Test
 {
     [TestClass]
-    public class LIbraryTest
+    public class LibraryTest
     {
+        Library library;
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            library = new Library();
+        }
+
+        [TestMethod]
+        public void TestAddBook()
+        {
+            library.AddBook(new CatalogEntry("hello", "world", true));
+
+            Assert.AreEqual(library.bookList.Count, 1);
+        }
+
+        [TestMethod]
+        public void TestAddEntry()
+        {
+            library.AddEntry("hello", "world", true);
+
+            Assert.AreEqual(Catalog.entries.Count, 3);
+        }
+
+        [TestMethod]
+        public void TestRemoveBook()
+        {
+            CatalogEntry catalogEntry = new CatalogEntry("hello", "world", true);
+
+            library.AddBook(catalogEntry);
+            library.RemoveBook(catalogEntry);
+
+            Assert.AreEqual(library.bookList.Count, 0);
+        }
+
         [TestMethod]
         public void TestCheckIfEntryExists()
         {
-            Library library = new Library();
-
             library.AddEntry("hello", "world", true);
 
             Assert.IsTrue(library.CheckIfEntryExists("hello", "world", true));
